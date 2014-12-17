e6c787cdc03c9f95a057ade22508eb3d7072c602
Tiago Correa <tiagocorrea_442@hotmail.com>
2014-12-17 16:37:43 -0300

    Alteração para puxar pelo codigo do funcionario

--- app/config/database.php
+++ app/config/database.php
@@ -65,7 +65,7 @@ return array(
 
 		'pgsql'     => array(
 			'driver'   => 'pgsql',
-			'host'     => '10.1.1.3',
+			'host'     => 'localhost',
 			'database' => 'sig_testes',
 			'username' => 'postgres',
 			'password' => 'aporedux',
